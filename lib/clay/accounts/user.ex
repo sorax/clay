40a90199c2a62a6b3306d5991e57249eb7adb530
sorax <sorax@lilith.local>
2022-05-13 21:21:51 +0200

    add admin flag to users

--- lib/clay/accounts/user.ex
+++ lib/clay/accounts/user.ex
@@ -7,6 +7,7 @@ defmodule Clay.Accounts.User do
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
+    field :admin, :boolean, default: false
 
     timestamps()
   end
