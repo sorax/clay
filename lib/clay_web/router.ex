b564f5f746921e385c722b8469f8fc454db85b7d
sorax <sorax@lilith.local>
2022-05-13 22:10:29 +0200

    add access-control

--- lib/clay_web/router.ex
+++ lib/clay_web/router.ex
@@ -1,7 +1,9 @@
 defmodule ClayWeb.Router do
   use ClayWeb, :router
 
+  import Phoenix.LiveDashboard.Router
   import ClayWeb.UserAuth
+  import ClayWeb.UserAccess
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -30,21 +32,12 @@ defmodule ClayWeb.Router do
   #   pipe_through :api
   # end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  # Admin-only section
+  scope "/", ClayWeb do
+    pipe_through [:browser, :require_admin]
 
-    scope "/" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: ClayWeb.Telemetry
-    end
+    # Enable LiveDashboard
+    live_dashboard "/dashboard", metrics: Telemetry
   end
 
   # Enables the Swoosh mailbox preview in development.
