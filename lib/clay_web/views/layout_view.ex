9970a6e973c3f86981a8c5d5c70acf8ba5f26b65
sorax <sorax@lilith.local>
2022-05-20 15:54:31 +0200

    update metatags & robots.txt

--- lib/clay_web/views/layout_view.ex
+++ lib/clay_web/views/layout_view.ex
@@ -1,7 +1,19 @@
 defmodule ClayWeb.LayoutView do
   use ClayWeb, :view
 
-  # Phoenix LiveDashboard is available only in development by default,
-  # so we instruct Elixir to not warn if the dashboard route is missing.
-  @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+  defp get_metatags(conn) do
+    {:safe, get_page_config(conn)}
+  end
+
+  defp get_page_config(%{path_info: ["claudia" | _], request_path: path}) do
+    ~s(<meta name="description" content=""/>
+    <meta name="robots" content="noindex,nofollow"/>
+    <link rel="canonical" href="https://hausgedacht.de#{path}"/>)
+  end
+
+  defp get_page_config(%{request_path: path}) do
+    ~s(<meta name="description" content=""/>
+    <meta name="robots" content="index,follow"/>
+    <link rel="canonical" href="https://hausgedacht.de#{path}"/>)
+  end
 end
