f9181872fa68375a633eca8d14c1d835a31db356
sorax <robert@hausgedacht.de>
2022-06-28 08:41:56 +0200

    add test for imprint

--- test/clay_web/controllers/page_controller_test.exs
+++ test/clay_web/controllers/page_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule ClayWeb.PageControllerTest do
   use ClayWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET startpage", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "hausgedacht"
+  end
+
+  test "GET imprint", %{conn: conn} do
+    conn = get(conn, "/impressum")
+    assert html_response(conn, 200) =~ "<h1>Impressum</h1>"
   end
 end
