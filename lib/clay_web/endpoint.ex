985547bce9c030b160a25c74bf16e9913727624e
sorax <robert@hausgedacht.de>
2022-08-07 23:14:51 +0200

    expose static files for safari

--- lib/clay_web/endpoint.ex
+++ lib/clay_web/endpoint.ex
@@ -20,7 +20,8 @@ defmodule ClayWeb.Endpoint do
     at: "/",
     from: :clay,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only:
+      ~w(assets fonts images favicon.ico robots.txt apple-touch-icon.png apple-touch-icon-precomposed.png)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
