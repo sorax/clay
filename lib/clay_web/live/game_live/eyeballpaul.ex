972448ac1c0438850d62d6fe52b22703845c34a0
sorax <robert@hausgedacht.de>
2022-09-09 22:53:40 +0200

    move qr logic

--- lib/clay_web/live/game_live/eyeballpaul.ex
+++ lib/clay_web/live/game_live/eyeballpaul.ex
@@ -1,10 +1,18 @@
 defmodule ClayWeb.GameLive.Eyeballpaul do
   use ClayWeb, :live_view
 
+  alias ClayWeb.Endpoint
+
   @impl true
   def mount(_params, _session, socket) do
+    qr =
+      (Endpoint.url() <> Routes.game_eyeballpaul_path(socket, :index))
+      |> EQRCode.encode()
+      |> EQRCode.svg(color: "#fff", viewbox: true, background_color: ":transparent")
+
     socket
     |> assign(page_title: "EyeBallPaul")
+    |> assign(qr: qr)
     |> reply(:ok)
   end
 end
