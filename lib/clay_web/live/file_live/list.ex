7684f0e8c3f56ebc390d7bfbf88f9a995933bf49
sorax <robert@hausgedacht.de>
2022-11-17 20:33:20 +0100

    Application.get_env is discouraged in module body

--- lib/clay_web/live/file_live/list.ex
+++ lib/clay_web/live/file_live/list.ex
@@ -1,7 +1,7 @@
 defmodule ClayWeb.FileLive.List do
   use ClayWeb, :live_view
 
-  @storage_path :clay |> Application.get_env(:storage) |> Keyword.fetch!(:path)
+  @storage_path :clay |> Application.compile_env(:storage) |> Keyword.fetch!(:path)
 
   @impl true
   def mount(_params, _session, socket) do
